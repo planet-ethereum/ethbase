ef8344c9b40e0c6992024fe7ce194f3adea1c3c7
Sina Mahmoodi <itz.s1na@gmail.com>
2018-09-17 20:03:49 +0200

    Add ropsten network with hd-wallet to truffle config
    Signed-off-by: Sina Mahmoodi <itz.s1na@gmail.com>

--- truffle.js
+++ truffle.js
@@ -1,25 +1,23 @@
-/*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
- * function when declaring them. Failure to do so will cause commands to hang. ex:
- * ```
- * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
- *     },
- *     network_id: '1',
- *     gas: 4500000,
- *     gasPrice: 10000000000,
- *   },
- */
+const HDWalletProvider = require('truffle-hdwallet-provider')
+
+const mnemonic = process.env.MNEMONIC
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   networks: {
     development: {
-      host: "127.0.0.1",
+      host: '127.0.0.1',
       port: 8545,
-      network_id: "dev" // Match any network id
+      network_id: '*' // Match any network id
+    },
+    ropsten: {
+      provider: function () {
+        return new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/')
+      },
+      network_id: '3',
+      gas: 6700000,
+      gasPrice: 2000000000 // 2 Gwei
     }
   }
-};
+}
