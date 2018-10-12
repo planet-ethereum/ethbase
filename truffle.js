d37debf9e245c68de1ed8bf800ca6c7fd285ab9f
Sina Mahmoodi <itz.s1na@gmail.com>
2018-10-12 14:46:20 +0200

    Update truffle configs
    Signed-off-by: Sina Mahmoodi <itz.s1na@gmail.com>

--- truffle.js
+++ truffle.js
@@ -3,13 +3,17 @@ const HDWalletProvider = require('truffle-hdwallet-provider')
 const mnemonic = process.env.MNEMONIC
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
   networks: {
+    ganache: {
+      host: '127.0.0.1',
+      port: 7545,
+      network_id: '*' // Match any network id,
+    },
     development: {
       host: '127.0.0.1',
       port: 8545,
-      network_id: '*' // Match any network id
+      network_id: '*', // Match any network id,
+      gas: 6000000
     },
     ropsten: {
       provider: function () {
@@ -18,6 +22,25 @@ module.exports = {
       network_id: '3',
       gas: 6700000,
       gasPrice: 2000000000 // 2 Gwei
+    },
+    rinkeby: {
+      provider: function () {
+        return new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/')
+      },
+      network_id: '4',
+      gas: 6700000,
+      gasPrice: 2000000000 // 2 Gwei
+    }
+  },
+  compilers: {
+    solc: {
+      version: '0.4.24'
+    }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
     }
   }
 }
